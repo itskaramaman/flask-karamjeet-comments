f22947c3d7c77061aa9aedc9a35a5730eb15ca2a
itskaramaman <45313619+itskaramaman@users.noreply.github.com>
2019-11-12 16:16:07 +0530

    changed name to email

--- main/forms/login.py
+++ main/forms/login.py
@@ -3,5 +3,5 @@ from wtforms import Form, StringField, PasswordField, validators
 
 class LoginForm(Form):
 	"""Login form for User."""
-	email = StringField('Name', [validators.Length(min=1)])
+	email = StringField('Email', [validators.Length(min=1)])
 	password = PasswordField('Password', [validators.Length(min=1)])
