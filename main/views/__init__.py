159b7477e7d5809e2579f38ece8cea147db404d0
itskaramaman <45313619+itskaramaman@users.noreply.github.com>
2019-11-15 15:17:59 +0530

    profile added to views/__init__

--- main/views/__init__.py
+++ main/views/__init__.py
@@ -3,3 +3,4 @@ from main.views.add_post import post_bp
 from main.views.register import register_bp
 from main.views.login import login_bp
 from main.views.logout import logout_bp
+from main.views.profile import profile_bp
