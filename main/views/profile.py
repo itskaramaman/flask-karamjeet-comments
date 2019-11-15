e97788c2e4fb7209cfbbe6cb29ea412291b0df3c
itskaramaman <45313619+itskaramaman@users.noreply.github.com>
2019-11-15 15:17:09 +0530

    profile business logic added

---
+++ main/views/profile.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template
+from flask_login import current_user, login_required
+from main.models import User, Post
+
+profile_bp = Blueprint('profile_bp', __name__)
+
+
+@profile_bp.route('/profile')
+@login_required
+def profile():
+	"""Author profile page."""
+	author = User.query.join(Post).filter_by(author_id=current_user.id).first()
+	print(dir(author))
+	return render_template('profile.html', author=author)
