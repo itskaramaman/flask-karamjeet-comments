7c7c35f9fdc122b7ab435a167baa97c50c72fc7b
itskaramaman <45313619+itskaramaman@users.noreply.github.com>
2019-11-15 15:31:35 +0530

    is_admin feature disabled

--- main/models/register.py
+++ main/models/register.py
@@ -7,7 +7,7 @@ class User(UserMixin, db.Model):
 	name = db.Column(db.String(255), nullable=False)
 	email = db.Column(db.String(255), nullable=False, unique=True)
 	password = db.Column(db.String(255), nullable=False)
-	is_admin = db.Column(db.Boolean, default=False)
+# 	is_admin = db.Column(db.Boolean, default=False)
 	posts =  db.relationship('Post', backref='user', lazy=True)
 
 	def __init__(self, name, email, password):
